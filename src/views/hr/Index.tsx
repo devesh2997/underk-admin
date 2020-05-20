460374c6add533c67432c71e2d2ad848a904b15d
Devesh Anand <ananddeves22@gmail.com>
2020-05-20 12:05:40 +0530

    init

---
+++ src/views/hr/Index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+// reactstrap components
+import {
+    Container,
+} from "reactstrap";
+
+export default class Admin extends React.Component<{}, {}> {
+
+    render() {
+        return (<Container>Test</Container>)
+    }
+
+}
